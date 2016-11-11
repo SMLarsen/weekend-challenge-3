d6531ca8603a7ab2af3ba0e031551c5fa2c9a530
Steve Larsen <stevelarsen01@gmail.com>
2016-11-11 15:32:31 -0600

    basic connections/file structure established

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// routing modules
+var index = require('./routes/index');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// routes
+// app.use('/bios', bios);
+// app.use('/likes', bios);
+
+// static files
+app.use('/', index);
+
+// Set port to listen to
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+    console.log("Server is listening on port: " + app.get('port'));
+});
